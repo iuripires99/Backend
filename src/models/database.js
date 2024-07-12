f63a9667013c11c8666126222d0f2dc953ee967d
iuripires99 <133361855+iuripires99@users.noreply.github.com>
2024-07-12 16:20:34 +0100

    Update database.js

--- src/models/database.js
+++ src/models/database.js
@@ -1,11 +1,11 @@
 var Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'divez_edve',
-    'divez_edve_user',
-    'tBPLEtXRjLtifuYzeipXdJ0w88bEItQm',
+    'divez_e30d',
+    'divez_e30d_user',
+    '9ZLWVOVr9jzG7vVpIv4PbAgCpkJW3F9q',
     {
-        host: 'dpg-cq8kei8gph6c73fmoi30-a',
+        host: 'dpg-cq8kie5ds78s73dl96g0-a',
         port: '5432',
         dialect: 'postgres'
     }
